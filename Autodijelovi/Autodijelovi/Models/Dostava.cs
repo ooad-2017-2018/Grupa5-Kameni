c814e46a822d5b017c3e86f44028b2c2093d703b
Adis Hodzic <ahodzic9@etf.unsa.ba>
2018-05-13 15:51:02 +0200

    Dodane forme za dostavu, katalog i narudzbu na servis, modificirana klasa Ddostava.

--- Autodijelovi/Autodijelovi/Models/Dostava.cs
+++ Autodijelovi/Autodijelovi/Models/Dostava.cs
@@ -12,20 +12,26 @@ namespace Autodijelovi.Models
         DateTime vrijemePolaska = new DateTime();
         List<DostavnoVozilo> dostavnoVozila = new List<DostavnoVozilo>();
         List<Artikal> artikli = new List<Artikal>();
+        string adresa;
+        Kupac kupac = new Kupac();
 
         public Dostava()
         {
 
         }
 
-        public Dostava(Dostavljac dostavljac, DateTime vrijemePolaska, List<DostavnoVozilo> dostavnoVozila, List<Artikal> artikli)
+        public Dostava(Kupac kupac, Dostavljac dostavljac, DateTime vrijemePolaska, List<DostavnoVozilo> dostavnoVozila, List<Artikal> artikli, string adresa)
         {
+            this.Kupac = kupac;
             this.Dostavljac = dostavljac;
             this.VrijemePolaska = vrijemePolaska;
             this.DostavnoVozila = dostavnoVozila;
             this.Artikli = artikli;
+            this.Adresa = adresa;
         }
 
+        public Kupac Kupac { get => kupac; set => kupac = value; }
+        public string Adresa { get => adresa; set => adresa = value; }
         public DateTime VrijemePolaska { get => vrijemePolaska; set => vrijemePolaska = value; }
         internal Dostavljac Dostavljac { get => dostavljac; set => dostavljac = value; }
         internal List<DostavnoVozilo> DostavnoVozila { get => dostavnoVozila; set => dostavnoVozila = value; }
