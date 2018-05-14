745b24d00b888c7322c20f6d443f213ce176df28
eherenda1 <eherenda1@etf.unsa.ba>
2018-05-14 04:29:23 +0200

    Aplikacija povezana sa bazom preko azure, kreirana jedna tabela u kojoj se smjestaju podaci kupaca koji su se registrovali za koristenje aplikacije. Treba dodati jos nekoliko tabela i povezati sa odgovarajucim formama.

--- Autodijelovi/Autodijelovi/KupacRegistrovanje.xaml.cs
+++ Autodijelovi/Autodijelovi/KupacRegistrovanje.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.UI.Popups;
+using Autodijelovi;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +28,31 @@ namespace Autodijelovi
         public KupacRegistrovanje()
         {
             this.InitializeComponent();
+        }
+
+        IMobileServiceTable<tabela> userTableObj = App.MobileService.GetTable<tabela>();
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tabela obj = new tabela();
+                obj.ime = imetekst.Text;
+                obj.prezime = prezimetekst.Text;
+                obj.korisnickoIme = kimetekst.Text;
+                obj.sifra = sifra.Text;
+
+                userTableObj.InsertAsync(obj);
+                MessageDialog msgDialog = new MessageDialog("Uspješno ste registrovani u sistem. Želimo van ugodno korištenje aplikacije Autodijelovi Kameni :) ");
+
+                msgDialog.ShowAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageDialog msgDialogError = new MessageDialog("Error : " +
+               ex.ToString());
+                msgDialogError.ShowAsync();
+            }
         }
     }
 }
---
+++ Autodijelovi/Autodijelovi/tabela.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autodijelovi
+{
+    class tabela
+    {
+        public string id
+        {
+            get;
+            set;
+        }
+        public string ime
+        {
+            get;
+            set;
+        }
+        public string prezime
+        {
+            get;
+            set;
+        }
+        public string korisnickoIme
+        {
+            get;
+            set;
+        }
+        public string sifra
+        {
+            get;
+            set;
+        }
+    }
+}
