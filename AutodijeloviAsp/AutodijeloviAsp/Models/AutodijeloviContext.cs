aefa2bca07c7e80c71d64fc6db0d5cd1d59372f6
eherenda1 <eherenda1@etf.unsa.ba>
2018-05-24 03:31:11 +0200

    Pocetak web aplikacije

---
+++ AutodijeloviAsp/AutodijeloviAsp/Models/AutodijeloviContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace AutodijeloviAsp.Models
+{
+    public class AutodijeloviContext : DbContext
+    {
+        public AutodijeloviContext() : base("DefaultConnection") 
+ {
+        }
+        //dodavanjem klasa iz modela kao DbSet iste će biti mapirane u bazu podataka
+        public DbSet<Kupac> Student { get; set; }
+        
+        //Ova funkcija se koriste da bi se ukinulo automatsko dodavanje množine u nazive
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ AutodijeloviAsp/AutodijeloviAsp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutodijeloviAsp.Startup))]
+namespace AutodijeloviAsp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
