26a737f09600f83d190ddcd8b3df89e3d582857e
Adis Hodzic <ahodzic9@etf.unsa.ba>
2018-05-26 14:42:11 +0200

    test 1

---
+++ Autodijelovi/Autodijelovi/ova.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autodijelovi
+{
+    class ova
+    {
+        private string ime;
+        public string prezime;
+    }
+}
